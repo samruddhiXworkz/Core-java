d137509e0f5e2064a4dbcefba99185237bbe6dec
samruddhiXworkz <samruddhikuri7@gmail.com>
2024-06-24 12:20:28 +0530

    26-06-2024

---
+++ BallPen.java
@@ -0,0 +1,34 @@
+class BallPen{
+  static String brand="Reynolds";
+  static String writingInstrument="BallpointPen";
+  static String color="Blue";
+  static String inkColor="Blue";
+  static int type=3;
+  
+ public static void main(String pen[]){
+
+  System.out.println("The Ballpen Strated");
+  System.out.println("The Ballpen brand is "+brand );
+  System.out.println("The Ballpen writingInstrument is " +writingInstrument);
+  System.out.println("The Ballpen color is "+color);
+  System.out.println("The Ballpen inkColor is "+ inkColor);
+  System.out.println("The Ballpen ended");
+
+  
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ }
+
+
+}
