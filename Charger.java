12d72fa86fc9ec40ce1778dd090e268098899485
samruddhiXworkz <samruddhikuri7@gmail.com>
2024-07-03 22:14:56 +0530

    03-07-2024

---
+++ Charger.java
@@ -0,0 +1,8 @@
+class Charger{
+ public static void main(String[] mi){
+ System.out.println("Fast Charging Wall Charger");
+ System.out.println("This is Certified charging adapter for your mobile phone.");
+ System.out.println("Order Cablebasket 3 amp Charger Wall Adapter ");
+ System.out.println("USB Charging Cables for All iOS & Android Devices (Black) online at Jiomart");
+ }
+}
