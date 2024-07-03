a28be8bdf1c8daf084e1efdae12c25f984eec25d
samruddhiXworkz <samruddhikuri7@gmail.com>
2024-07-03 20:32:48 +0530

    basic java code

---
+++ Bottle.java
@@ -0,0 +1,9 @@
+class Bottle{
+ public static void main(String[] bottle){
+ System.out.println("Alkaline Ionized water with an optimum pH of 9.");
+ System.out.println("Ionizing breaks the molecules and restructures them into micro clusters.");
+ System.out.println("This micro clustered water can be more easily absorbed into the cells of human body,");
+ System.out.println("30mcg Per Liter. It contains Natural Mineral Electrolytes as residues.");
+
+ }
+}
