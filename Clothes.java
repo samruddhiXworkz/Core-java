2c90b1a35e56bac80d4d4aa5c999a5435682ad69
samruddhiXworkz <samruddhikuri7@gmail.com>
2024-07-03 22:31:55 +0530

    03-07-2024

---
+++ Clothes.java
@@ -0,0 +1,12 @@
+class Clothes
+{
+  public static void main(String[] hm)
+  {
+    System.out.println("Clothes are important for Dayly usage");
+	System.out.println("Sunny Dress, Sizzle in this rolled collar dress ");
+	System.out.println("charm your folks in a sunny silhouette");
+    System.out.println("This structured lyocell dress has been designed to make you feel comfortable and look astounding");
+
+
+    }
+}
