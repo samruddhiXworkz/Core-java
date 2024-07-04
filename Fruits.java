bcae14ed234b5d9c636202b33035c56db109115c
samruddhiXworkz <samruddhikuri7@gmail.com>
2024-07-04 09:39:29 +0530

    03-07-2024

---
+++ Fruits.java
@@ -0,0 +1,9 @@
+class Fruits{
+  public static void main(String[] mango){
+  System.out.println("May Support Heart Health.");
+  System.out.println("Boost the Immune System. ...");
+  System.out.println("Improve Skin Health. ...");
+  System.out.println("May Ease Constipation.");
+
+  }
+}
